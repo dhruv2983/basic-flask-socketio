b31ba0d232b8b6d8781574c8bca9a2e4816d636b
Dhruv <dbhatt2983@gamil.com>
2024-05-18 19:18:38 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@socketio.on("message")
+def handle_message(data):
+    emit("message", data + 5)
+
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=8000, debug=True)
